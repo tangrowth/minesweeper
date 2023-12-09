e32349f48b043e86fc0e2ffe1c5c665a15c6e637
tangrowth <to.chinatsu@icloud.com>
2023-12-10 00:29:01 +0900

    feat: セルの値の設定

--- src/components/Game/Cell.tsx
+++ src/components/Game/Cell.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const Cell: React.FC = () => {
+interface CellProps {
+  isOpend: boolean;
+  isBomb: boolean;
+  isFragged: boolean;
+  count: number;
+}
+
+const Cell: React.FC<CellProps> = ({ isOpend, isBomb, isFragged, count }) => {
   return (
     <div className="Cell">
-
+      <span>
+        {isOpend && !isBomb && count > 0 && <span>{count}</span>}
+        {isOpend && isBomb && <span>💣</span>}
+        {!isOpend && isFragged && <span>🚩</span>}
+      </span>
     </div>
   );
-}
+};
 
 export default Cell;
